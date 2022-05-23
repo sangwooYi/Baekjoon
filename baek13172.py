b274f6605e99b93d8bf866dc45db691c17ed09b3
unknown <okqwaszx12@naver.com>
2022-05-23 21:05:05 +0900

    0523 study

--- baek13172.py
+++ baek13172.py
@@ -8,6 +8,12 @@ sys.stdin = open("baek13172.txt")
 
 모듈러 역원 b-1 은 modN에서 b * b-1 의 나머지가 1 이 되게하는 값 
 즉 b = 3이고 모듈러 X 가 11일때, b-1은 4  (3*4 는 12이므로 1mod11 임)
+
+Q = Si/Ni 
+이때 Ni*(-1) = (mod+1)//Ni 
+따라서 (((mod+1)//Ni)*Si) % mod 가 우리가 원하는 답 
+
+이거 나중에 풀자 ㅠㅠ
 """
 
 
@@ -15,3 +21,9 @@ M = int(input())
 arr = [0] * M
 for i in range(0, M):
     arr[i] = list(map(int, input().split()))
+
+mod = 1000000007
+a = 3
+# 이게 역원
+a_m1 = (mod+1) // a
+print((a_m1*7)%mod)
