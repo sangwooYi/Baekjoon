5d500c70e97329e19f7a86c13cff42ac583f94d7
unknown <okqwaszx12@naver.com>
2022-05-30 20:56:49 +0900

    0530 study

---
+++ baek8980.py
@@ -0,0 +1,42 @@
+import sys
+sys.stdin = open("baek8980.txt")
+"""
+도착지점 순으로 정렬
+싣는 지점 => 내리는 지점
+까지의 capa 배열 (수용 가능한 화물양)을 체크
+그 사이에서의 최솟값 == 해당 구간에서 운송가능한 최대 운송치
+이 값과, 현재 실을 수 있는 cost중에 최솟값을 사용
+(음수가 되면 안되므로)
+
+이거 너무 어렵다.. 풀이봐도 잘 모르겠네
+
+무조건 다시 풀어볼 것..
+"""
+
+# 총 노드 , 최대 화물 갯수
+N, C = map(int, input().split())
+M = int(input())
+MAP = [0] * M
+for i in range(0, M):
+    MAP[i] = list(map(int, input().split()))
+MAP.sort(key=lambda x : x[1])
+# 마을의 수용가능양 체크
+capa = [C] * N
+answer = 0
+for i in range(0, len(MAP)):
+    s, e, cost = MAP[i]
+    temp = C
+    # s, 와 e 사이에서 capa[j]를 체크해서
+    # 가장 작은 값이 실질적으로 운송 가능한 화물양
+    for j in range(s, e):
+        # 얘는 s - e 구간 사이에서 운송 가능한 최대 물량
+        temp = min(temp, capa[j])
+    # capa 배열이 음수가 되면 안되니까!
+    # 즉 해당 구간에서 운송 가능한 최대 물량 vs 현재 포인트에서 실을 수 있는 최대 물량
+    # 이 값중에서 최솟값을 취해야 capa배열이 음수가 되지 않는다! 
+    # => 이게 운송 가능한 최대치가 되는것
+    temp = min(temp, cost)
+    for j in range(s, e):
+        capa[j] -= temp
+    answer += temp
+print(answer)
